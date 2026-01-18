93727b5a74267bc9c7dbea17052b1354b7e7736f
Fussita <hualong.chiang@gmail.com>
2026-01-18 15:50:41 -0400

    upd

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,19 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { SeedService } from './seeds/seed.service';
-
-import * as crypto from "crypto";
-(global as any).crypto = crypto;
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { SeedService } from './seeds/seed.service'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { logger: ['log', 'error', 'warn', 'debug', 'verbose'] });
+  const app = await NestFactory.create(AppModule, { logger: ['log', 'error', 'warn', 'debug', 'verbose'] })
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true, 
     transform: true,       
-  }));
+  }))
   
   const seedService = app.get(SeedService) 
   await seedService.run()
@@ -23,11 +20,11 @@ async function bootstrap() {
   .setDescription('Documentación de la API con Swagger') 
   .setVersion('1.0')
   .addBearerAuth( { type: 'http', scheme: 'bearer', bearerFormat: 'JWT', }, 'access-token' )  
-  .build();
+  .build()
   
-  const document = SwaggerModule.createDocument(app, config); 
-  SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument(app, config) 
+  SwaggerModule.setup('api', app, document)
   
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000)
 }
-bootstrap();
+bootstrap()
